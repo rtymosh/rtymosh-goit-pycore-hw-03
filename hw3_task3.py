7036088179de42f13104f2e48eb792f2bef08df2
Tymoshenko Roman <tymoshenkorv@gmail.com>
2025-11-09 02:11:09 +0200

    hw Task 3 - added solution and comments

--- hw3_task3.py
+++ hw3_task3.py
@@ -19,7 +19,8 @@
 
 # Параметр функції phone_number — це рядок з телефонним номером у різноманітних форматах.
 # Функція видаляє всі символи, крім цифр та символу '+'.
-# Якщо міжнародний код відсутній, функція додає код '+38'. Це враховує випадки, коли номер починається з '380' (додається лише '+') та коли номер починається без коду (додається '+38').
+# Якщо міжнародний код відсутній, функція додає код '+38'. Це враховує випадки, коли номер починається з '380' (додається лише '+')
+# та коли номер починається без коду (додається '+38').
 # Функція повертає нормалізований телефонний номер у вигляді рядка.
 
 
@@ -58,6 +59,34 @@
 # Нормалізовані номери телефонів для SMS-розсилки: ['+380671234567',
 # '+380952345678', '+380441234567', '+380501234567', '+380501233234',
 # '+380503451234', '+380508889900', '+380501112222', '+380501112211']
+import re
 
 
-    
+def normalize_phone(phone_number: str) -> str:
+    # delete all characters except digits and '+'
+    filtered_numbers = re.sub(r"[^\d+]", "", phone_number)
+    if filtered_numbers.startswith("+38"):
+        # already has the correct ukrainian code
+        formatted_phone_number = filtered_numbers
+    else:
+        # add the ukrainian code if missing and it is no matter if '+' is present or not
+        formatted_phone_number = "+38" + filtered_numbers.lstrip("+")
+
+    return formatted_phone_number
+
+
+raw_numbers = [
+    "067\\t123 4567",
+    "(095) 234-5678\\n",
+    "+380 44 123 4567",
+    "380501234567",
+    "    +38(050)123-32-34",
+    "     0503451234",
+    "(050)8889900",
+    "38050-111-22-22",
+    "38050 111 22 11   ",
+]
+
+# Test the function
+sanitized_numbers = [normalize_phone(num) for num in raw_numbers]
+print("Нормалізовані номери телефонів для SMS-розсилки:", sanitized_numbers)
